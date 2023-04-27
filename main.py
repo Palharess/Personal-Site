02177ce03741936df4c8049a0fb12d98c934132e
Gabriel Palhares <gabrielpalhares334@gmail.com>
2023-04-27 11:05:19 -0300

    Progression and Skills page

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
@@ -7,6 +7,18 @@ Bootstrap(app)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/skills")
+def skills():
+    return render_template("skills.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
