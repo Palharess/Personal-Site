e5b8e5940927b32d86307e0a14e5fbac63ae4604
Gabriel Palhares <gabrielpalhares334@gmail.com>
2023-04-30 00:35:29 -0300

    Quase pronto

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 from flask import Flask, render_template, url_for
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+from flask_ckeditor import CKEditor
+
 
 app = Flask(__name__)
 Bootstrap(app)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+ckeditor = CKEditor(app)
+
+
+
+class MyForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/contact")
+@app.route("/contact", methods = ["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    form = MyForm()
+    return render_template("contact.html", form=form)
 
 @app.route("/projects")
 def projects():
